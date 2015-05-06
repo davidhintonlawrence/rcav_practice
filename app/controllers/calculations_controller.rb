bb120cda60b914457467550984ba8083c3a3a5fa
davidhintonlawrence <dlawrence2016@kellogg.northwestern.edu>
2015-05-06 14:16:31 -0500

    TA SESSION

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,27 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
-  def sqrt
+
+  def square
+    @result = params["number"].to_i**2
+    # render 'instructions' to send to instructions view
   end
 
-  def pmt
+  def square_root
+    @result = Math.sqrt(params["number"].to_i).round(2)
+    # Math module called on. rounded to 2 decimail points at the end
+    render 'square'
   end
-end
+
+
+  def pmt
+    # @apr = params["interest_rate".to_f
+    #   @payments = params["number_of_payments"].to_i
+    #   @principal = params["principal_value"].to_f
+
+    #   @monthly_payment = (rate * @principal)/(1 - (1 + rate)**-nper)
+
+    end
+
+
+  end
